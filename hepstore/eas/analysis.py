1b468a7618510eef269a390dc70a1cdd27f4fc42
PeterSchichtel <peterschichtel@gmx.de>
2017-06-16 16:59:47 +0200

    restructure multiprocessing

--- hepstore/eas/analysis.py
+++ hepstore/eas/analysis.py
@@ -103,8 +103,8 @@ class analysis:
                 pass
             # create event
             ev=event()
-            #ev.particles_from_file(pfile)
-            #ev.xmax_from_file(xfile)
+            ev.particles_from_file(pfile)
+            ev.xmax_from_file(xfile)
             # analyse event
             for analysis in self.analysis:
              #   analysis.analyse(ev)
