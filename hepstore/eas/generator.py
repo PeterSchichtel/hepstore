f1b616649809503e0ce2255079f51d3d61c6165f
PeterSchichtel <peterschichtel@gmx.de>
2017-07-01 15:14:45 +0100

    restructure eas genartor

--- hepstore/eas/generator.py
+++ hepstore/eas/generator.py
@@ -5,125 +5,45 @@ import glob
 import shutil
 
 from hepstore.tools import *
-import energy,element,process,final,model
-import h7
+import importlib
 
 class Generator(object):
     
     def __init__(self,options):
-        self.options   = options
-        self.path      = os.getcwd()
-        self.energy    = energy.Energy(1.0e+06)
-        self.element   = element.Element('proton')
-        self.process   = process.Process('qcd')
-        self.generator = 'h7'
-        self.model     = 'frac'
-        self.final     = final.Final('lightquark')
+        self.options        = options
+        self.path           = os.getcwd()
         pass
     
     def run(self,path):
-        print "--generator: working on '%s'" % path
-        args           = os.path.normpath(path).split('/')
-        self.path      = path
-        self.energy    = energy.Energy(args[0])
-        self.element   = element.Element(args[1])
-        self.process   = process.Process(args[2])
-        self.generator = args[3]
-        self.model     = args[4]
-        self.final     = final.Final(args[5])
         if self.options.regenerate:
             shutil.rmtree(os.path.join(path,'mc_generation'))
             shutil.rmtree(os.path.join(path,'events'))
             pass
         mkdir(os.path.join(path,'mc_generation'))
-        mkdir(os.path.join(path,'mc_generation',self.next_runfolder()))
         mkdir(os.path.join(path,'events'))
-        # generate runcard
-        self.run_card()
-        # perfom MC genaration
-        self.generate()
-        # convert hepmc -> corsika
+        # generate events in hepmc format
+        self.path           = path
+        lib                 = importlib.import_module(os.path.normpath(path).split('/')[3])
+        app                 = lib.Generator(self.options)
+        self.hepmc_filepath = app.run(os.path.join(path,'mc_generation')
+        # convert events
         self.convert()
-        # apply nucleus model
+        # enrich with nucleons
         self.enrich()
         pass
 
-    def next_runfolder(self):
-        try:
-            count=0
-            while count in [ int(item.split("_")[-1]) for item in glob.glob(os.path.join(self.path,'mc_generation','run_*'))]:
-                count+=1
-                pass
-            self.run_folder = "run_%i" % count
-            return self.run_folder
-        except ValueError as err:
-            print [ item.split("_")[-1] for item in glob.glob(os.path.join(self.path,'mc_generation','run_*'))]
-            print err
-            raise err
-        pass
-
-    def target_energy(self):
-        return self.energy.value/self.element.nucleons
-
-    def run_card(self):
-        if self.generator == "h7":
-            self.run_card = h7.Card( self.target_energy(), self.process.process, self.final.h7(), os.path.join(self.path,'mc_generation') )
-            self.run_card.produce()
-            pass
-        else:
-            raise NotImplemented("unknown generator '%s'" % self.generator)
-        pass
-
-    def next_seed(self):
-        # use the given random seed
-        if self.options.regenerate or not os.path.exists(os.path.join(self.path,'mc_generation','seeds.dat')):
-            with open(os.path.join(self.path,'mc_generation','seeds.dat'),'w') as fout:
-                fout.write("%i\n" % self.options.seed)
-                pass
-            return self.options.seed
-        # generate a new random seed 
-        else:
-            count=1
-            with open(os.path.join(self.path,'mc_generation','seeds.dat'),'r') as fin:
-                while count in map(int, fin.readlines() ):
-                    count+=1
-                    pass
-                pass
-            with open(os.path.join(self.path,'mc_generation','seeds.dat'),'a') as fout:
-                fout.write("%i\n" % count)
-                pass
-            return count
-        pass
-
-    def generate(self):
-        if self.generator == 'h7':
-            from  hepstore.docker import herwig as herwig
-            #build
-            print "--generator: herwig build"
-            herwig.run([ '--directory', os.path.join(self.path,'mc_generation'),
-                         'build'    , '%s.in'  % self.run_card.name, ])
-            #integrate
-            print "--generator: herwig integrate"
-            herwig.run([ '--directory', os.path.join(self.path,'mc_generation'),
-                         'integrate', '%s.run' % self.run_card.name, ])
-            #run
-            os.link( os.path.abspath( os.path.join(self.path,'mc_generation','Herwig-scratch') ),
-                  os.path.abspath( os.path.join(self.path,'mc_generation',self.run_folder,'Herwig-scratch') )
-            )
-            os.link( os.path.abspath( os.path.join(self.path,'mc_generation','%s.run' % self.run_card.name) ),
-                  os.path.abspath( os.path.join(self.path,'mc_generation',self.run_folder,'%s.run' % self.run_card.name) )
-            )
-            print "--generator: herwig run"
-            herwig.run([ '--directory', os.path.join(self.path,'mc_generation',self.run_folder),
-                         'run'      , '%s.run' % self.run_card.name, '-N', '%i' % self.options.nevents, '-s', '%i' % self.next_seed() ])
-            pass
-        else:
-            raise NotImplemented("unknown generator '%s'" % self.generator)
-        pass
-
     def convert(self):
+        print "--generator: convert to corsika format"
         from hepstore.docker import hepmc2corsika as converter
-        converter.run([ '--directory', os.path.join(self.path,'mc_generation',self.run_folder), '-f', '%s.hepmc' % self.run_card.name, '-o', 'event' ])
+        converter.run([ '--directory', os.path.dirname(self.hepmc_filepath), '-f', '%s' % os.path.basename(self.hepmc_filepath), '-o', 'event' ])
+        pass
+
+    def enrich(self):
+        print "--generator: apply nucleon interaction model"
+        for fname in glob.glob(os.path.join(os.path.dirname(self.hepmc_file),'event-*')):
+            model.enrich( fname, self.next_name(fname),
+                           energy=self.energy, element=self.element, model=self.model)
+            pass
         pass
 
     def next_name(self,fname):
@@ -137,16 +57,7 @@ class Generator(object):
             while count in list_of_nums:
                 count+=1
                 pass
-            ##print  os.path.join( self.path, 'events', "%s-%i" % ( "-".join(os.path.basename(fname).split("-")[:-1]),count) )
             return os.path.join( self.path, 'events', "%s-%i" % ( "-".join(os.path.basename(fname).split("-")[:-1]),count) )
-        pass
-
-    def enrich(self):
-        print "--generator: apply nucleon interaction model"
-        for fname in glob.glob(os.path.join(self.path,'mc_generation',self.run_folder,'event-*')):
-            model.enrich( fname, self.next_name(fname),
-                           energy=self.energy, element=self.element, model=self.model)
-            pass
         pass
     
     pass
--- hepstore/eas/h7.py
+++ hepstore/eas/h7.py
@@ -1,85 +1,158 @@
-
+#!/usr/bin/env python
 
 import os
+import hepstore.docker.herwig as herwig
 
-class Card(object):
+import element,final`
 
-    def __init__(self,energy,process,final,path):
-        self.energy  = energy
-        self.process = process
-        self.final   = final
-        self.path    = path
+def runcard(fpath,energy,process,final):
+    name = os.path.basename(fpath)
+    with open( os.path.join("%s.in" % fpath), 'w' ) as fcard:
+        fcard.write( "##################################################\n" )
+        fcard.write( "## Collider type                                  \n" )
+        fcard.write( "read Matchbox/PPCollider.in                       \n" )
+        fcard.write( "                                                  \n" )
+        fcard.write( "##################################################\n" )
+        fcard.write( "## Beam energy sqrt(s)                            \n" )
+        fcard.write( "cd /Herwig/EventHandlers                          \n" )
+        fcard.write( "set EventHandler:LuminosityFunction:Energy     %.2f*GeV\n" % (energy+1.0) )
+        fcard.write( "set EventHandler:LuminosityFunction:BeamEMaxA  %.2f*GeV\n" %  energy      )
+        fcard.write( "set EventHandler:LuminosityFunction:BeamEMaxB  1.0*GeV \n"                )
+        fcard.write( "                                                  \n" )
+        fcard.write( "##################################################\n" )
+        fcard.write( "## Model assumptions                              \n" )
+        fcard.write( "read Matchbox/StandardModelLike.in                \n" )
+        fcard.write( "read Matchbox/DiagonalCKM.in                      \n" )
+        fcard.write( "## Set the order of the couplings                 \n" )
+        fcard.write( "cd /Herwig/MatrixElements/Matchbox                \n" )
+        if process == "qcd":
+            fcard.write( "set Factory:OrderInAlphaS 2                       \n" )
+            fcard.write( "set Factory:OrderInAlphaEW 0                      \n" )
+            fcard.write( "## Select the process                             \n" )
+            fcard.write( "do Factory:Process p p -> %s                      \n" % final )
+            fcard.write( "                                                  \n" )
+            fcard.write( "##################################################\n" )
+            fcard.write( "## Cut selection                                  \n" )
+            fcard.write( "read Matchbox/DefaultPPJets.in                    \n" )
+            fcard.write( "insert JetCuts:JetRegions 0 FirstJet              \n" )
+            fcard.write( "insert JetCuts:JetRegions 1 SecondJet             \n" )
+            pass
+        elif process == "zlo":
+            raise NotImplemented("unknown process '%s'" % process)
+        #pass
+        else:
+            raise NotImplemented("unknown process '%s'" % process)
+        fcard.write( "                                                  \n" )
+        fcard.write( "##################################################\n" )
+        fcard.write( "##Matrix element provider                         \n" )
+        fcard.write( "read Matchbox/MadGraph-MadGraph.in                \n" )
+        fcard.write( "##################################################\n" )
+        fcard.write( "## Scale choice                                   \n" )
+        fcard.write( "cd /Herwig/MatrixElements/Matchbox/               \n" )
+        fcard.write( "set Factory:ScaleChoice /Herwig/MatrixElements/Matchbox/Scales/SHatScale\n" )
+        fcard.write( "                                                  \n" )
+        fcard.write( "##################################################\n" )
+        fcard.write( "## Matching and shower selection                  \n" )
+        fcard.write( "read Matchbox/LO-DefaultShower.in                 \n" )
+        fcard.write( "                                                  \n" )
+        fcard.write( "##################################################\n" )
+        fcard.write( "## PDF choice                                     \n" )
+        fcard.write( "read Matchbox/FourFlavourScheme.in                \n" )
+        fcard.write( "read Matchbox/MMHT2014.in                         \n" )
+        fcard.write( "                                                  \n" )
+        fcard.write( "##################################################\n" )
+        fcard.write( "## HepMC output                                   \n" )
+        fcard.write( "insert /Herwig/Generators/EventGenerator:AnalysisHandlers 0 /Herwig/Analysis/HepMCFile\n" )
+        fcard.write( "set /Herwig/Analysis/HepMCFile:PrintEvent 10000   \n" )
+        fcard.write( "set /Herwig/Analysis/HepMCFile:Format GenEvent    \n" )
+        fcard.write( "set /Herwig/Analysis/HepMCFile:Units GeV_mm       \n" )
+        fcard.write( "                                                  \n" )
+        fcard.write( "##################################################\n" )
+        fcard.write( "## Save the generator                             \n" )
+        fcard.write( "do /Herwig/MatrixElements/Matchbox/Factory:ProductionMode\n" )
+        fcard.write( "cd /Herwig/Generators                             \n" )
+        fcard.write( "saverun %s EventGenerator                         \n" % name)
+        pass #with
+    pass
+
+class Generator(object):
+
+    def __init__(self,options):
+        self.options = options
+        self.seed    = options.seed
+        self.folder  = os.getcwd()
         self.name    = 'herwig'
         pass
 
-    def produce(self):
-        with open(os.path.join(self.path,'%s.in' % self.name),'w') as fcard:
-            fcard.write( "##################################################\n" )
-            fcard.write( "## Collider type\n" )
-            fcard.write( "read Matchbox/PPCollider.in\n" )
-            fcard.write( "\n" )
-            fcard.write( "##################################################\n" )
-            fcard.write( "## Beam energy sqrt(s)\n" )
-            fcard.write( "cd /Herwig/EventHandlers\n" )
-            fcard.write( "set EventHandler:LuminosityFunction:Energy %.2f*GeV\n" % (self.energy+1.0) )
-            fcard.write( "set EventHandler:LuminosityFunction:BeamEMaxA  %.2f*GeV\n" % self.energy )
-            fcard.write( "set EventHandler:LuminosityFunction:BeamEMaxB     1.0*GeV\n")
-            fcard.write( "\n" )
-            fcard.write( "##################################################\n" )
-            fcard.write( "## Model assumptions\n" )
-            fcard.write( "read Matchbox/StandardModelLike.in\n" )
-            fcard.write( "read Matchbox/DiagonalCKM.in\n" )
-            fcard.write( "## Set the order of the couplings\n" )
-            fcard.write( "cd /Herwig/MatrixElements/Matchbox\n" )
-            if self.process == "qcd":
-                fcard.write( "set Factory:OrderInAlphaS 2\n" )
-                fcard.write( "set Factory:OrderInAlphaEW 0\n" )
-                fcard.write( "## Select the process\n" )
-                fcard.write( "do Factory:Process p p -> %s\n" % self.final )
-                fcard.write( "\n" )
-                fcard.write( "##################################################\n" )
-                fcard.write( "## Cut selection\n" )
-                fcard.write( "read Matchbox/DefaultPPJets.in\n" )
-                fcard.write( "insert JetCuts:JetRegions 0 FirstJet\n" )
-                fcard.write( "insert JetCuts:JetRegions 1 SecondJet\n" )
+    def run(self,path):
+        args           = os.path.normpath(path).split('/')
+        self.path      = path
+        self.energy    = float(args[0])
+        self.element   = element.Element(args[1])
+        self.process   = args[2]
+        self.generator = args[3]
+        self.model     = args[4]
+        self.final     = final.Final(args[5])
+        print "--h7: working on '%s'" % path
+        # create directory
+        mkdir( path )
+        # create runcard
+        print "--h7: runcard"
+        runcard( os.path.join(path,self.name), self.target_energy(), self.process, self.final.h7() )
+        #build
+        print "--h7: build"
+        herwig.run([ '--directory', path,
+                     'build'    , '%s.in'  % self.name, ])
+        #integrate
+        print "--h7: integrate"
+        herwig.run([ '--directory', path,
+                     'integrate', '%s.run' % self.name, ])
+        # find a clean generation folder
+        self.next()
+        mkdir( os.path.join( path, self.folder))
+        os.link( os.path.abspath( os.path.join(path,'Herwig-scratch') ),
+                 os.path.abspath( os.path.join(path,self.folder,'Herwig-scratch') )
+        )
+        os.link( os.path.abspath( os.path.join(path,'%s.run' % self.name) ),
+                 os.path.abspath( os.path.join(path,self.folder,'%s.run' % self.name) )
+        )
+        # run
+        print "--h7: run"
+        herwig.run([ '--directory', os.path.join(path,self.folder),
+                     'run'      , '%s.run' % self.name, '-N', '%i' % self.options.nevents, '-s', '%i' % self.seed ])
+        # return the path to the hepmc file
+        return os.path.join( path, self.folder, '%s.hepmc' % self.name )
+
+    def next(self):
+        # use the given random seed
+        if self.options.regenerate or not os.path.exists(os.path.join(self.path,'seeds.dat')):
+            with open(os.path.join(self.path,'seeds.dat'),'w') as fout:
+                fout.write("%i\n" % self.options.seed)
                 pass
-            elif self.process == "zlo":
-                raise NotImplemented("unknown process '%s'" % self.process)
-                #pass
-            else:
-                raise NotImplemented("unknown process '%s'" % self.process)
-            fcard.write( "\n" )
-            fcard.write( "##################################################\n" )
-            fcard.write( "##Matrix element provider\n" )
-            fcard.write( "read Matchbox/MadGraph-MadGraph.in\n" )
-            fcard.write( "##################################################\n" )
-            fcard.write( "## Scale choice\n" )
-            fcard.write( "cd /Herwig/MatrixElements/Matchbox/\n" )
-            fcard.write( "set Factory:ScaleChoice /Herwig/MatrixElements/Matchbox/Scales/SHatScale\n" )
-            fcard.write( "\n" )
-            fcard.write( "##################################################\n" )
-            fcard.write( "## Matching and shower selection\n" )
-            fcard.write( "read Matchbox/LO-DefaultShower.in\n" )
-            fcard.write( "\n" )
-            fcard.write( "##################################################\n" )
-            fcard.write( "## PDF choice\n" )
-            fcard.write( "read Matchbox/FourFlavourScheme.in\n" )
-            fcard.write( "read Matchbox/MMHT2014.in\n" )
-            fcard.write( "\n" )
-            fcard.write( "##################################################\n" )
-            fcard.write( "## HepMC output\n" )
-            fcard.write( "insert /Herwig/Generators/EventGenerator:AnalysisHandlers 0 /Herwig/Analysis/HepMCFile\n" )
-            fcard.write( "set /Herwig/Analysis/HepMCFile:PrintEvent 10000\n" )
-            fcard.write( "set /Herwig/Analysis/HepMCFile:Format GenEvent\n" )
-            fcard.write( "set /Herwig/Analysis/HepMCFile:Units GeV_mm\n" )
-            fcard.write( "\n" )
-            fcard.write( "##################################################\n" )
-            fcard.write( "## Save the generator\n" )
-            fcard.write( "do /Herwig/MatrixElements/Matchbox/Factory:ProductionMode\n" )
-            fcard.write( "cd /Herwig/Generators\n" )
-            fcard.write( "saverun %s EventGenerator\n" % self.name)
-            pass #with
+            self.seed = self.options.seed
+        # generate a new random seed 
+        else:
+            count=1
+            with open(os.path.join(self.path,'seeds.dat'),'r') as fin:
+                while count in map(int, fin.readlines() ):
+                    count+=1
+                    pass
+                pass
+            with open(os.path.join(self.path,'seeds.dat'),'a') as fout:
+                fout.write("%i\n" % count)
+                pass
+            self.seed = count
+            pass
+        # find a clean folder to run
+        count=0
+        while count in [ int(item.split("_")[-1]) for item in glob.glob(os.path.join(self.path,'run_*'))]:
+            count+=1
+            pass
+        self.folder = "run_%i" % count
         pass
 
+    def target_energy(self):
+        return self.energy/self.element.nucleons
+    
     pass
             
